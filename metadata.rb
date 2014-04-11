676d74bbfbf6cb7b31b1bba5fdf7a40daa4f44bb
Gary Leong <gwleong@gmail.com>
2014-04-10 17:38:04 -0700

    added 41 test

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures passenger'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-#test 41
+#test 42
 
 depends "apt", "2.3.8"
 depends "build-essential", "2.0.0"
